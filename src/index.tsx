f9126c017f8ce2eba7142608d25c6bc4a30e8728
amjed-ali-k <amjedmgm@gmail.com>
2024-12-18 12:39:56 +0530

    feat: add schema

--- src/index.tsx
+++ src/index.tsx
@@ -5,30 +5,45 @@ import { Info } from "./compontents/info";
 import { CertificateViewer } from "./compontents/certificateViewer";
 import { generateSVGFromElement } from "./lib";
 import { SampleCert } from "./compontents/sample-cert";
+import { createDb } from "./db";
 
-const app = new Hono();
+type Bindings = {
+  DB: D1Database;
+};
 
-app.use(renderer);
+type Variables = {
+  db: ReturnType<typeof createDb>;
+};
 
-app.get("/", (c) => {
-  return c.render(
-    <Layout>
-      <div class="grid grid-cols-4 flex-1 !h-full">
-        <CertificateViewer />
-        <Info />
-      </div>
-    </Layout>
-  );
-});
-
-app.get("/image.svg", async (c) => {
-  const v = await generateSVGFromElement((<SampleCert />) as any, ['Pacifico']);
-  c.res.headers.set("Content-Type", "image/svg+xml");
-  return new Response(v, {
-    headers: {
-      "Content-Type": "image/svg+xml; charset=utf-8",
-    },
+const app = new Hono<{
+  Bindings: Bindings;
+  Variables: Variables;
+}>()
+  .use(renderer)
+  .use("*", async (c, next) => {
+    c.set("db", createDb(c.env.DB));
+    await next();
+  })
+  .get("/", (c) => {
+    return c.render(
+      <Layout>
+        <div class="grid grid-cols-4 flex-1 !h-full">
+          <CertificateViewer />
+          <Info />
+        </div>
+      </Layout>
+    );
+  })
+  .get("/image.svg", async (c) => {
+    const v = await generateSVGFromElement((<SampleCert />) as any, [
+      "Pacifico",
+    ]);
+    c.res.headers.set("Content-Type", "image/svg+xml");
+    return new Response(v, {
+      headers: {
+        "Content-Type": "image/svg+xml; charset=utf-8",
+      },
+    });
   });
-});
 
 export default app;
