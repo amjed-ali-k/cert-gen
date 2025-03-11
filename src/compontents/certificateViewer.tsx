2c6feacac99757fa265fce6dea84b7b945340a8a
amjed-ali-k <amjedmgm@gmail.com>
2025-03-12 02:57:10 +0530

    fix: urls

--- src/compontents/certificateViewer.tsx
+++ src/compontents/certificateViewer.tsx
@@ -3,7 +3,7 @@ export const CertificateViewer = async ({id}: {id: string}) => {
     <div class="p-3 col-span-4 md:col-span-3">
       <div class="border-4 p-2 md:p-4">
         <div class="h-fit w-full">
-          <img class="w-full" src={`/cert/${id}/image.svg`} />
+          <img class="w-full" src={`https://cert-image.amjedmgm.workers.dev/cert/${id}/image.svg`} />
         </div>
       </div>
     </div>
--- src/compontents/info.tsx
+++ src/compontents/info.tsx
@@ -23,7 +23,7 @@ export const Info = ({
         </div>
         <div class="mt-3">
           <a
-            href={`/cert/${id}/image.png`}
+            href={`https://cert-image.amjedmgm.workers.dev/cert/${id}/image.png`}
             target={"_blank"}
             id="downloadButton"
             className="px-4 mr-2 py-2 rounded-md border border-black bg-white text-black text-sm hover:shadow-[4px_4px_0px_0px_rgba(0,0,0)] transition duration-200"
